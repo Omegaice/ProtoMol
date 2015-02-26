db543bfb59e27f1d08fe2b18082189929a5cf1ed
James Sweet <james.sweet88@googlemail.com>
2015-02-26 12:37:43 -0500

    Corrected naming of checkpoint files

--- protomol/module/CheckpointModule.cpp
+++ protomol/module/CheckpointModule.cpp
@@ -11,61 +11,39 @@ using namespace std;
 using namespace ProtoMol;
 
 void CheckpointModule::init(ProtoMolApp *app) {
-  OutputFactory &f = app->outputFactory;
-  f.registerExemplar(new OutputCheckpoint());
+	OutputFactory &f = app->outputFactory;
+	f.registerExemplar(new OutputCheckpoint());
 }
 
 void CheckpointModule::configure(ProtoMolApp *app) {
-  Configuration &config = app->config;
-
-  if (!(enabled = config.valid("Checkpoint"))) return;
-
-  config["CheckpointPosBase"] = "";
-  config["CheckpointVelBase"] = "";
-
-  if( config.valid("gromacstprfile") ) {
-    const string temp = config["gromacstprfile"];
-    config["CheckpointPosBase"] = temp.substr(0, temp.rfind('.')+1);
-    config["CheckpointVelBase"] = temp.substr(0, temp.rfind('.')+1);
-  }else{
-    const string temp = config["posfile"];
-    config["CheckpointPosBase"] = temp.substr(0, temp.rfind('.') + 1);
-
-    if (!config.valid("velfile")) {
-        config["CheckpointVelBase"] = config["CheckpointPosBase"];
-    } else {
-        const string tempv = config["velfile"];
-        config["CheckpointVelBase"] = tempv.substr(0, tempv.rfind('.') + 1);
-    }
-  }
 }
 
 void CheckpointModule::read(ProtoMolApp *app) {
-  if (!enabled) return;
+	if( !enabled ) { return; }
 
-  Configuration &config = app->config;
+	Configuration &config = app->config;
 
-  std::cout << "Reading Checkpoint Base Data" << std::endl;
+	std::cout << "Reading Checkpoint Base Data" << std::endl;
 
-  CheckpointConfigReader confReader;
-  if (confReader.open(config["Checkpoint"], ios::in)){
-    confReader.readBase(config, Random::Instance());
-  }
+	CheckpointConfigReader confReader;
+	if( confReader.open(config["Checkpoint"], ios::in)) {
+		confReader.readBase(config, Random::Instance());
+	}
 }
 
 void CheckpointModule::postBuild(ProtoMolApp *app) {
-  if (!enabled) return;
+	if( !enabled ) { return; }
 
-  Configuration &config = app->config;
+	Configuration &config = app->config;
 
-  std::cout << "Reading Checkpoint Integrator Data" << std::endl;
-  // Load integrator data
-  CheckpointConfigReader confReader;
-  if (confReader.open(config["Checkpoint"], ios::in)){
-    confReader.readIntegrator(app->integrator);
-  }
+	std::cout << "Reading Checkpoint Integrator Data" << std::endl;
+	// Load integrator data
+	CheckpointConfigReader confReader;
+	if( confReader.open(config["Checkpoint"], ios::in)) {
+		confReader.readIntegrator(app->integrator);
+	}
 }
 
-string CheckpointModule::WithoutExt(const string &path){
-  return path.substr(0, path.rfind(".") + 1);
+string CheckpointModule::WithoutExt(const string &path) {
+	return path.substr(0, path.rfind(".") + 1);
 }
